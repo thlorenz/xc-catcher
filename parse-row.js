6946f48faab7c9508f8bc22a3d784db11af4f485
Thorsten Lorenz <thlorenz@gmx.de>
2014-09-29 21:37:28 -0400

    improving handling of table rows with missing columns and resolving actual mp3 link

--- parse-row.js
+++ parse-row.js
@@ -14,26 +14,58 @@ function clean(s) {
 }
 
 function textColFrom(cols, idx) {
-  return clean(cols[idx].children[0].data)
+  var col = cols[idx];
+  if (!col) return 'N/A';
+  var children = col.children;
+  if (!children.length) return 'N/A';
+  return clean(children[0].data)
 }
 
 function getCommonName(cols) {
   var commonNameInfo = cols.find('.common-name')
-  return clean(commonNameInfo.find('a')[0].children[0].data)
+  try {
+    return clean(commonNameInfo.find('a')[0].children[0].data)
+  } catch (err) {
+    return 'getCommonName -- ERROR -- ' + err.toString();
+  }
 }
 
 function getScientificName(cols) {
   var scientificNameInfo = cols.find('.scientific-name')
-  return clean(scientificNameInfo[0].children[0].data);
-  
+  try {
+    return clean(scientificNameInfo[0].children[0].data);
+  } catch (err) {
+    return 'getScientificName  -- ERROR -- ' + err.toString();
+  }
 }
 
 function getLocation(cols) {
   // some locations are linked, but not all
   var locationInfo = cheerio(cols[7]).find('a')
-  return locationInfo && locationInfo.length 
-    ? clean(locationInfo[0].children[0].data)
-    : textColFrom(cols, 7);
+  try {
+    return locationInfo && locationInfo.length 
+      ? clean(locationInfo[0].children[0].data)
+      : textColFrom(cols, 7);
+  } catch (err) {
+    return 'getLocation  -- ERROR -- ' + err.toString();
+  }
+}
+
+function getDownloadLink(cols) {
+  var el = cheerio(cols[0]).find('.jp-type-single');
+  if (!el) return 'N/A';
+  return el.data('xc-filepath');
+}
+
+function getRating(cols) {
+  if (cols.length < 12) return 'N/A';
+
+  var actionsInfo = cheerio(cols[11])
+    , downloadLink = actionsInfo.find('a')[0].attribs.href
+    , ratingInfo = actionsInfo.find('li.selected')
+    , rating = cheerio(ratingInfo).find('span')[0].children[0].data
+
+  return rating;
 }
 
 
@@ -46,20 +78,17 @@ var go = module.exports = function parseRow(rowHtml) {
     return textColFrom(cols, idx)
   }
 
-  var commonName = getCommonName(cols) 
+  var commonName     = getCommonName(cols)
     , scientificName = getScientificName(cols)
-    , date      = textCol(4)
-    , time      = textCol(5)
-    , country   = textCol(6)
-    , loc       = getLocation(cols)
-    , elevation = textCol(8)
-    , type      = textCol(9)
+    , date           = textCol(4)
+    , time           = textCol(5)
+    , country        = textCol(6)
+    , loc            = getLocation(cols)
+    , elevation      = textCol(8)
+    , type           = textCol(9)
+    , downloadLink   = getDownloadLink(cols)
+    , rating         = getRating(cols)
  
-  var actionsInfo = cheerio(cols[11])
-    , downloadLink = actionsInfo.find('a')[0].attribs.href
-    , ratingInfo = actionsInfo.find('li.selected')
-    , rating = cheerio(ratingInfo).find('span')[0].children[0].data
-
   return { 
       commonName     : commonName
     , scientificName : scientificName
@@ -73,5 +102,3 @@ var go = module.exports = function parseRow(rowHtml) {
     , rating         : rating
   }
 }
-
-// Test
--- test/parse-row.js
+++ test/parse-row.js
@@ -4,6 +4,10 @@ var test = require('tap').test
   , parseRow = require('../parse-row')
   , fs = require('fs')
 
+function inspect(obj, depth) {
+  console.error(require('util').inspect(obj, false, depth || 5, true));
+}
+
 test('\nrow with all info and all links', function (t) {
   var rowHtml = fs.readFileSync(__dirname + '/fixtures/row-all-links.html', 'utf8')
   var parsed = parseRow(rowHtml)
@@ -17,7 +21,7 @@ test('\nrow with all info and all links', function (t) {
         loc: 'Copalinga Lodge (Zamora - Zamora Chinchipe)',
         elevation: '950',
         type: 'song',
-        downloadLink: '/176277/download',
+        downloadLink: 'http://www.xeno-canto.org/sounds/uploaded/RJNSQXBIJE/XC176277-9.%20Tinamus%20tao%20XC.MP3',
         rating: 'A' } 
     , 'parses all info correclty'
   )
@@ -37,8 +41,28 @@ test('\nrow with missing info and missing location link', function (t) {
         loc: 'Reserva Yaguarundi, Misiones',
         elevation: '?',
         type: 'song',
-        downloadLink: '/49402/download',
+        downloadLink : 'http://www.xeno-canto.org/sounds/uploaded/JQSYKZHEMB/FINCH%20Uniform_02_Reserva%20Yaguarundi_Misiones_AR_16MAY04_Rosendo%20Fraga.mp3',
         rating: 'B' } 
+    , 'parses all info correclty'
+  )
+  t.end()
+})
+
+test('\nrow with less than 12 columns', function (t) {
+  var rowHtml = fs.readFileSync(__dirname + '/fixtures/row-less-than-12-cols.html', 'utf8')
+  var parsed = parseRow(rowHtml)
+  t.deepEqual(
+      parsed
+    , { commonName: 'Grey Tinamou',
+        scientificName: 'Tinamus tao kleei',
+        date: '2014-04-26',
+        time: '06:30',
+        country: 'Ecuador',
+        loc: 'Copalinga Lodge (Zamora - Zamora Chinchipe)',
+        elevation: '950',
+        type: 'song',
+        downloadLink: 'http://www.xeno-canto.org/sounds/uploaded/RJNSQXBIJE/XC176277-9.%20Tinamus%20tao%20XC.MP3',
+        rating: 'N/A' } 
     , 'parses all info correclty'
   )
   t.end()
